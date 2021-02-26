0dfa3fa6996a001031f569cddf8840130ca3d314
Jevon Charles <jevoncharles@me.com>
2021-02-26 15:02:55 -0500

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by Jevon Charles on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
