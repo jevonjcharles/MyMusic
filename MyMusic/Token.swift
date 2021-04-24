403ec67cc98d6af34bc08a27de27d295dc5c5740
Jevon Charles <jevoncharles@me.com>
2021-04-24 14:42:34 -0400

    Token

---
+++ MyMusic/Token.swift
@@ -0,0 +1,10 @@
+//
+//  Token.swift
+//  MyMusic
+//
+//  Created by Jevon Charles on 4/24/21.
+//
+
+import Foundation
+
+let developerToken = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkszWUc5S1AzSEYifQ.eyJpYXQiOjE2MTQzNzQ2MjAsImV4cCI6MTYyOTkyNjYyMCwiaXNzIjoiNVlORDZVOTg0MyJ9.EsNTjJrj4Uye6ji5e-Wxc9PKYrWGJv9L5AcaazLtNavBpuURbALsYAi0GGBd0aTVWjykeVc8X9WGq2_l4jPFcA"
